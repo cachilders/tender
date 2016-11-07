3444c88a1297abbb8f54dbd34e735480a75d2959
Casey Childers <cachilders@gmail.com>
2016-11-07 09:00:53 -0800

    [bug out] Repair minor css render issue

--- resources/src/views/Home.vue
+++ resources/src/views/Home.vue
@@ -5,8 +5,6 @@
       <h1>TENDER</h1>
       <h2>Submission Tracking for Authors</h2>
       <h2>Coming Soon</h2>
-    </div>
-    <div class="__footer">
       <ul>
         <li>
           <a href="https://twitter.com/cachilders" target="_blank">
