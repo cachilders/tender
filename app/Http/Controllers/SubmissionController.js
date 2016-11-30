86a508a833200a2037b985543301686bf24715d4
Casey Childers <cachilders@gmail.com>
2016-11-30 14:28:51 -0800

    [feature] Route index of subs for current user

--- app/Http/Controllers/SubmissionController.js
+++ app/Http/Controllers/SubmissionController.js
@@ -5,8 +5,10 @@ const Submission = use('App/Model/Submission');
 class SubmissionController {
 
   * index (request, response) {
+    let user = request.cookie('user', null);
+    console.log(request.cookie('user', null));
     try {
-      const submissions = yield Submission.all();
+      const submissions = yield Submission.query().where('user_id', user);
       response.status(200).json(submissions);
     } catch (e) {
       response.send(e.message);
